34e9994bade8612b8c770d91ef27ffa9431969e6
Sagar Duhan <sagar.duhan1@gmail.com>
2018-05-23 00:53:10 +0530

    Initial Commit

---
+++ stackatada-stackTests/stackatada_stackTests.swift
@@ -0,0 +1,36 @@
+//
+//  stackatada_stackTests.swift
+//  stackatada-stackTests
+//
+//  Created by Sagar Duhan on 23/05/18.
+//  Copyright © 2018 Sagar Duhan. All rights reserved.
+//
+
+import XCTest
+@testable import stackatada_stack
+
+class stackatada_stackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
